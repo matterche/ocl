c24a3185e471a567e10e59cc3d604acf8a66fb76
ewillink <ewillink>
2010-01-18 08:57:40 +0000

    Collections mostly working

--- tests/org.eclipse.ocl.ecore.tests/src/org/eclipse/ocl/ecore/tests/AbstractTestSuite.java
+++ tests/org.eclipse.ocl.ecore.tests/src/org/eclipse/ocl/ecore/tests/AbstractTestSuite.java
@@ -14,7 +14,7 @@
  *
  * </copyright>
  *
- * $Id: AbstractTestSuite.java,v 1.24.2.3 2010/01/15 07:39:53 ewillink Exp $
+ * $Id: AbstractTestSuite.java,v 1.24.2.4 2010/01/18 08:57:40 ewillink Exp $
  */
 
 package org.eclipse.ocl.ecore.tests;
@@ -108,6 +108,7 @@ public abstract class AbstractTestSuite
 	public static void suite(CheckedTestSuite result) {
 		result.createTestSuite(EvaluateOclAnyOperationsTest.class, "Evaluate OclAny Operations Tests");
 		result.createTestSuite(EvaluateBooleanOperationsTest.class, "Evaluate Boolean Operations Tests");
+		result.createTestSuite(EvaluateCollectionOperationsTest.class, "Evaluate Collection Operations Tests");
 		result.createTestSuite(EvaluateNumericOperationsTest.class, "Evaluate Numeric Operations Tests");
 		result.createTestSuite(EvaluateStringOperationsTest.class, "Evaluate String Operations Tests");
 
---
+++ tests/org.eclipse.ocl.ecore.tests/src/org/eclipse/ocl/ecore/tests/EvaluateCollectionOperationsTest.java
@@ -0,0 +1,48 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2009 Eclipse Modeling Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   E.D.Willink - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: EvaluateCollectionOperationsTest.java,v 1.1.2.1 2010/01/18 08:57:40 ewillink Exp $
+ */
+
+package org.eclipse.ocl.ecore.tests;
+
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EEnum;
+import org.eclipse.emf.ecore.EEnumLiteral;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EParameter;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.ocl.ecore.CallOperationAction;
+import org.eclipse.ocl.ecore.Constraint;
+import org.eclipse.ocl.ecore.SendSignalAction;
+import org.eclipse.ocl.tests.GenericEvaluateCollectionOperationsTest;
+
+/**
+ * Tests for evaluate collection expressions.
+ */
+public class EvaluateCollectionOperationsTest
+extends GenericEvaluateCollectionOperationsTest<EObject, EPackage, EClassifier, EClassifier, EClass, EDataType, EClassifier, EEnum, EOperation, EParameter, EStructuralFeature,
+EAttribute, EReference, EEnumLiteral, EObject, CallOperationAction, SendSignalAction, Constraint> {
+
+	@Override
+	public EcoreTestReflection getStaticReflection() {
+		return EcoreTestReflection.INSTANCE;
+	}
+}
