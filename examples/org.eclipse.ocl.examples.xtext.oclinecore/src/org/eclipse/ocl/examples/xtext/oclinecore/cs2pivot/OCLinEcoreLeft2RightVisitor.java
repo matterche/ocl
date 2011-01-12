cc67b389374921eb7561750086ac668724699a70
ewillink <ewillink>
2011-01-12 10:28:58 +0000

    IteratorTests 50% functional, 2 normal tests fail

--- examples/org.eclipse.ocl.examples.xtext.oclinecore/src/org/eclipse/ocl/examples/xtext/oclinecore/cs2pivot/OCLinEcoreLeft2RightVisitor.java
+++ examples/org.eclipse.ocl.examples.xtext.oclinecore/src/org/eclipse/ocl/examples/xtext/oclinecore/cs2pivot/OCLinEcoreLeft2RightVisitor.java
@@ -12,24 +12,22 @@
  *
  * </copyright>
  *
- * $Id: OCLinEcoreLeft2RightVisitor.java,v 1.1.2.2 2011/01/07 12:13:23 ewillink Exp $
+ * $Id: OCLinEcoreLeft2RightVisitor.java,v 1.1.2.3 2011/01/12 10:28:58 ewillink Exp $
  */
 package org.eclipse.ocl.examples.xtext.oclinecore.cs2pivot;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.ocl.examples.pivot.Constraint;
 import org.eclipse.ocl.examples.pivot.ExpressionInOcl;
+import org.eclipse.ocl.examples.pivot.Feature;
 import org.eclipse.ocl.examples.pivot.MonikeredElement;
 import org.eclipse.ocl.examples.pivot.OclExpression;
-import org.eclipse.ocl.examples.pivot.Operation;
 import org.eclipse.ocl.examples.pivot.PivotFactory;
 import org.eclipse.ocl.examples.pivot.PivotPackage;
-import org.eclipse.ocl.examples.pivot.Property;
 import org.eclipse.ocl.examples.pivot.Type;
 import org.eclipse.ocl.examples.pivot.Variable;
 import org.eclipse.ocl.examples.pivot.utilities.PivotUtil;
-import org.eclipse.ocl.examples.xtext.base.baseCST.OperationCS;
-import org.eclipse.ocl.examples.xtext.base.baseCST.StructuralFeatureCS;
+import org.eclipse.ocl.examples.xtext.base.baseCST.FeatureCS;
 import org.eclipse.ocl.examples.xtext.base.baseCST.TypeCS;
 import org.eclipse.ocl.examples.xtext.base.cs2pivot.CS2PivotConversion;
 import org.eclipse.ocl.examples.xtext.essentialocl.cs2pivot.EssentialOCLLeft2RightVisitor;
@@ -59,14 +57,18 @@ public class OCLinEcoreLeft2RightVisitor
 			pivotElement.setContextVariable(contextVariable);
 		}
 		EObject eContainer = csConstraint.eContainer();
-		if (eContainer instanceof OperationCS) {
-			Operation contextOperation = PivotUtil.getPivot(Operation.class, (OperationCS)eContainer);
-			context.setType(contextVariable, contextOperation.getClass_());
+		if (eContainer instanceof FeatureCS) {
+			Feature contextFeature = PivotUtil.getPivot(Feature.class, (FeatureCS)eContainer);
+			context.setType(contextVariable, contextFeature.getFeaturingClass());
 		}
-		else if (eContainer instanceof StructuralFeatureCS) {
-			Property contextProperty = PivotUtil.getPivot(Property.class, (StructuralFeatureCS)eContainer);
-			context.setType(contextVariable, contextProperty.getClass_());
-		}
+//		if (eContainer instanceof OperationCS) {
+//			Operation contextOperation = PivotUtil.getPivot(Operation.class, (OperationCS)eContainer);
+//			context.setType(contextVariable, contextOperation.getClass_());
+//		}
+//		else if (eContainer instanceof StructuralFeatureCS) {
+//			Property contextProperty = PivotUtil.getPivot(Property.class, (StructuralFeatureCS)eContainer);
+//			context.setType(contextVariable, contextProperty.getClass_());
+//		}
 		else if (eContainer instanceof TypeCS) {
 			Type contextType = PivotUtil.getPivot(Type.class, (TypeCS)eContainer);
 			context.setType(contextVariable, contextType);
