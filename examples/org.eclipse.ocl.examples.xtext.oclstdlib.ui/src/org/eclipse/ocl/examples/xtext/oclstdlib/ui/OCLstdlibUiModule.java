4cb91e480b48f98ee99de7d748a925e1e5001c82
ewillink <ewillink>
2011-01-21 14:06:16 +0000

    Re-instate Hovers

--- examples/org.eclipse.ocl.examples.xtext.oclstdlib.ui/src/org/eclipse/ocl/examples/xtext/oclstdlib/ui/OCLstdlibUiModule.java
+++ examples/org.eclipse.ocl.examples.xtext.oclstdlib.ui/src/org/eclipse/ocl/examples/xtext/oclstdlib/ui/OCLstdlibUiModule.java
@@ -5,12 +5,14 @@ package org.eclipse.ocl.examples.xtext.oclstdlib.ui;
 
 import org.eclipse.ocl.examples.xtext.base.pivot2cs.BaseLocationInFileProvider;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.model.BaseDocument;
+import org.eclipse.ocl.examples.xtext.essentialocl.ui.model.BaseEObjectTextHover;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.syntaxcoloring.BaseAntlrTokenToAttributeIdMapper;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.syntaxcoloring.EssentialOCLHighlightingConfiguration;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.syntaxcoloring.EssentialOCLSemanticHighlightingCalculator;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.xtext.common.types.access.jdt.IJavaProjectProvider;
 import org.eclipse.xtext.resource.ILocationInFileProvider;
+import org.eclipse.xtext.ui.editor.hover.DispatchingEObjectTextHover;
 import org.eclipse.xtext.ui.editor.model.XtextDocument;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.AbstractAntlrTokenToAttributeIdMapper;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
@@ -27,6 +29,10 @@ public class OCLstdlibUiModule extends AbstractOCLstdlibUiModule
 
 	public Class<? extends AbstractAntlrTokenToAttributeIdMapper> bindAbstractAntlrTokenToAttributeIdMapper() {
 		return BaseAntlrTokenToAttributeIdMapper.class;
+	}
+	
+	public Class<? extends DispatchingEObjectTextHover> bindDispatchingEObjectTextHover() {
+		return BaseEObjectTextHover.class;
 	}
 
 	public Class<? extends ILocationInFileProvider> bindILocationInFileProvider() {
@@ -49,8 +55,4 @@ public class OCLstdlibUiModule extends AbstractOCLstdlibUiModule
 	public Class<? extends XtextDocument> bindXtextDocument() {
 		return BaseDocument.class;
 	}
-
-//	public Class<? extends XtextSourceViewerConfiguration> bindXtextSourceViewerConfiguration() {
-//		return BaseSourceViewerConfiguration.class;
-//	}
 }
