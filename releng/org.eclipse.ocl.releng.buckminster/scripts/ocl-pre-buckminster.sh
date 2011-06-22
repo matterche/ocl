4512f63155d345882fe9a081ddf835e1a9facc89
Adolfo SBH <adolfosbh@opencanarias.com>
2011-06-22 17:58:53 +0100

    [349114] adolfosbh - [project] Migrate to GIT - Sources are not properly fetched by hudson, some changes to make it possible.

--- releng/org.eclipse.ocl.releng.buckminster/scripts/ocl-pre-buckminster.sh
+++ releng/org.eclipse.ocl.releng.buckminster/scripts/ocl-pre-buckminster.sh
@@ -40,6 +40,7 @@ COMPACT_BUILD_ID="${COMPACT_BUILD_ID:0:12}"
 	echo "buckminster.output.root=${WORKSPACE}/buildroot/buckminster.output"
 	echo "buckminster.temp.root=${WORKSPACE}/buildroot/buckminster.temp"
 	echo "eclipse.staging.area=/shared/download-staging.priv/modeling/mdt/ocl"
+	echo "git.ocl.local.repository.location=${WORKSPACE}/org.eclipse.ocl.git"
 
 	echo
 	echo "# Sign the jars (added by Hudson job definition)"
@@ -47,13 +48,13 @@ COMPACT_BUILD_ID="${COMPACT_BUILD_ID:0:12}"
 
 	echo
 	echo "# Eclipse local download area (added by Hudson job definition"
-        echo "eclipse.download=file:/home/data/httpd/download.eclipse.org"
+	echo "eclipse.download=file:/home/data/httpd/download.eclipse.org"
 	echo
 	echo "# Required Properties for Publishing (added by Hudson job definition)"
 	echo "packages.base=MDT-OCL.downloads"
-        echo "tests.base=MDT-OCL.test.results"
-        echo "javadoc.base=MDT-OCL.javadoc"
-        echo "version=3.1.0"
+	echo "tests.base=MDT-OCL.test.results"
+	echo "javadoc.base=MDT-OCL.javadoc"
+	echo "version=3.1.0"
 	echo "downloads.area=/home/data/httpd/download.eclipse.org/modeling/mdt/ocl"
 
 
