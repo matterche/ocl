415b0c37f97023a4c42ebd76ab96bf9a45d483c4
Adolfo SBH <adolfosbh@opencanarias.com>
2011-06-22 18:08:21 +0100

    [349114] adolfosbh - [project] Migrate to GIT - ocl-pre-buckminster.sh lost executability again >.<

--- releng/org.eclipse.ocl.releng.buckminster/scripts/ocl-pre-buckminster.sh
+++ releng/org.eclipse.ocl.releng.buckminster/scripts/ocl-pre-buckminster.sh
@@ -52,7 +52,7 @@ COMPACT_BUILD_ID="${COMPACT_BUILD_ID:0:12}"
 	echo
 	echo "# Required Properties for Publishing (added by Hudson job definition)"
 	echo "packages.base=MDT-OCL.downloads"
-	echo "tests.base=MDT-OCL.test.results"
+	echo "tests.base=MDT-OCL.test.results" 
 	echo "javadoc.base=MDT-OCL.javadoc"
 	echo "version=3.1.0"
 	echo "downloads.area=/home/data/httpd/download.eclipse.org/modeling/mdt/ocl"
