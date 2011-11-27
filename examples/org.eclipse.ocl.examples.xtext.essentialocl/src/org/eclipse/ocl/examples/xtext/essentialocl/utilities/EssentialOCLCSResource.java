177380c70c6a1e08185c25dc475811fd1b0b4940
Ed.Willink <ed@willink.me.uk>
2011-11-27 18:44:12 +0000

    [unrelated] Eliminate use of ModelRegistry
    Conflicts:
    	tests/org.eclipse.ocl.examples.xtext.tests/META-INF/MANIFEST.MF

--- examples/org.eclipse.ocl.examples.xtext.essentialocl/src/org/eclipse/ocl/examples/xtext/essentialocl/utilities/EssentialOCLCSResource.java
+++ examples/org.eclipse.ocl.examples.xtext.essentialocl/src/org/eclipse/ocl/examples/xtext/essentialocl/utilities/EssentialOCLCSResource.java
@@ -17,23 +17,12 @@
 package org.eclipse.ocl.examples.xtext.essentialocl.utilities;
 
 import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.log4j.Logger;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.ocl.examples.modelregistry.eclipse.ModelResourceResolver;
-import org.eclipse.ocl.examples.modelregistry.environment.ModelResolver;
-import org.eclipse.ocl.examples.modelregistry.environment.ProjectHandle;
-import org.eclipse.ocl.examples.modelregistry.standalone.JavaProjectHandle;
-import org.eclipse.ocl.examples.modelregistry.standalone.ModelFileResolver;
 import org.eclipse.ocl.examples.pivot.NamedElement;
 import org.eclipse.ocl.examples.pivot.Type;
 import org.eclipse.ocl.examples.pivot.evaluation.EvaluationContext;
@@ -53,11 +42,8 @@ import org.eclipse.xtext.util.CancelIndicator;
 public class EssentialOCLCSResource extends LazyLinkingResource
 	implements BaseCSResource, EvaluationContext
 {	
-	private static final Logger logger = Logger.getLogger(EssentialOCLCSResource.class);
-
 	private PivotEnvironment environment = null;
 	private NamedElement specificationContext = null;
-	private ModelResolver modelResolver = null;
 	
 	public EssentialOCLCSResource() {
 		super();
@@ -116,28 +102,12 @@ public class EssentialOCLCSResource extends LazyLinkingResource
 	public PivotEnvironment getEnvironment() {
 		return environment;
 	}
-
-	public ModelResolver getModelResolver() {
-		return modelResolver;
-	}
 	
 	public NamedElement getSpecificationContext() {
 		return specificationContext;
 	}
 
 	public URI resolve(URI uri) {
-		try {
-			ModelResolver resolver = useModelResolver();
-			if (resolver != null) {
-				java.net.URI javaURI = new java.net.URI(uri.toString());
-				URI resolvedURI = resolver.getURI(javaURI);
-				if (resolvedURI != null) {
-					uri = resolvedURI;
-				}
-			}
-		} catch (Exception e) {
-			logger.warn("Failed to resolve '" + uri + "'", e);
-		}
 		URI csURI = getURI();
 		if (csURI.isRelative()) {
 			File csRelative = new File(csURI.toFileString());
@@ -273,68 +243,5 @@ public class EssentialOCLCSResource extends LazyLinkingResource
 
 	public void setEnvironment(PivotEnvironment environment) {
 		this.environment = environment;
-	}
-
-	public void setModelResolver(ModelResolver modelResolver) {
-		this.modelResolver = modelResolver;
-	}
-
-//	public void setSpecificationContext(NamedElement specificationContext) {
-//		this.specificationContext = specificationContext;
-//	}
-
-	public ModelResolver useModelResolver() throws IOException {
-		if (modelResolver == null) {
-			URI uri = getURI();
-			if (uri.isPlatformResource()) {
-				String path = uri.toPlatformString(true);
-				IResource workspaceResource = ResourcesPlugin.getWorkspace().getRoot().findMember(new Path(path));
-				modelResolver = new ModelResourceResolver(workspaceResource);
-			}
-			else if (uri.isPlatformPlugin()) {
-				return null;
-			}
-			else {
-				String fileName = uri.toFileString();
-				File file = new File(fileName);
-				FilenameFilter filter = new FilenameFilter()
-				{
-					public boolean accept(File dir, String name) {
-						if (name.equals(ProjectHandle.DEFAULT_MODEL_REGISTRY_NAME)) {
-							return true;
-						}
-						if (name.equals(".settings")) {
-							return true;
-						}
-						return false;
-					}
-				};
-				File projectFolder = null;
-				File registryFile = null;
-				for (File folder = file.getParentFile(); folder != null; folder = folder.getParentFile()) {
-					File[] listFiles = folder.listFiles(filter);
-					if ((listFiles != null) && (listFiles.length > 0)) {
-						projectFolder = folder;
-						registryFile = listFiles[0];
-						break;
-					}
-				}
-				String registryPath = null;
-				if ((registryFile != null) && registryFile.isDirectory()) {
-					File[] listFiles = registryFile.listFiles(filter);
-					if ((listFiles != null) && (listFiles.length > 0)) {
-						registryPath = ".settings/" + ProjectHandle.DEFAULT_MODEL_REGISTRY_NAME;
-					}
-				}
-				else {
-					registryPath = ProjectHandle.DEFAULT_MODEL_REGISTRY_NAME;
-				}
-				if ((projectFolder != null) && (registryPath != null)) {
-					JavaProjectHandle project = new JavaProjectHandle(projectFolder, registryPath);
-					modelResolver = new ModelFileResolver(project, file);
-				}
-			}
-		}
-		return modelResolver;
 	}
 }
