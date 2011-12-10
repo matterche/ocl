8a275d037fa86a0b859c88a828cd44da384805bf
Ed.Willink <ed@willink.me.uk>
2011-12-10 14:03:53 +0000

    [unrelated] Conditionalize test on availablility of UML 2.5 files

--- tests/org.eclipse.ocl.examples.xtext.tests/src/org/eclipse/ocl/examples/test/xtext/RoundTripTests.java
+++ tests/org.eclipse.ocl.examples.xtext.tests/src/org/eclipse/ocl/examples/test/xtext/RoundTripTests.java
@@ -357,7 +357,10 @@ public class RoundTripTests extends XtextTestCase
 	}
 
 	public void testCompleteOCLRoundTrip_UML() throws IOException, InterruptedException {
-		doRoundTripFromCompleteOCL(URI.createPlatformResourceURI("/UML-2.5/XMI-23-Nov-2001/Semanticed UML.ocl", true));
+		URI uml_2_5 = URI.createPlatformResourceURI("/UML-2.5/XMI-23-Nov-2001/Semanticed UML.ocl", true);
+		if (uml_2_5.isFile()) {
+			doRoundTripFromCompleteOCL(uml_2_5);
+		}
 //		doRoundTripFromCompleteOCL(URI.createPlatformResourceURI("/UML-2.5/XMI-23-Nov-2001/UML.xmi", true));
 	}
 
