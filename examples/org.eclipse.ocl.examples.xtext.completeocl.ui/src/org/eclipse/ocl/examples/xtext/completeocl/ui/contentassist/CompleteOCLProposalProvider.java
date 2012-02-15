293139d7beef96367e99105aaa4695a800434ea0
Ed.Willink <ed@willink.me.uk>
2012-02-15 10:08:52 +0000

    [345282] Support proposals for A::B::C references

--- examples/org.eclipse.ocl.examples.xtext.completeocl.ui/src/org/eclipse/ocl/examples/xtext/completeocl/ui/contentassist/CompleteOCLProposalProvider.java
+++ examples/org.eclipse.ocl.examples.xtext.completeocl.ui/src/org/eclipse/ocl/examples/xtext/completeocl/ui/contentassist/CompleteOCLProposalProvider.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2010 E.D.Willink and others.
+ * Copyright (c) 2010,2012 E.D.Willink and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,26 +11,14 @@
  *     E.D.Willink - initial API and implementation
  *
  * </copyright>
- *
- * $Id: CompleteOCLProposalProvider.java,v 1.1 2010/04/13 06:36:42 ewillink Exp $
  */
 package org.eclipse.ocl.examples.xtext.completeocl.ui.contentassist;
 
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EReference;
-import org.eclipse.jface.text.contentassist.ICompletionProposal;
-import org.eclipse.ocl.examples.xtext.base.baseCST.SimpleNamedElementRefCS;
-import org.eclipse.ocl.examples.xtext.essentialocl.essentialOCLCST.ExpCS;
-import org.eclipse.ocl.examples.xtext.essentialocl.essentialOCLCST.InfixExpCS;
-import org.eclipse.ocl.examples.xtext.essentialocl.essentialOCLCST.NavigationOperatorCS;
-import org.eclipse.xtext.CrossReference;
 import org.eclipse.xtext.RuleCall;
-import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext;
 import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
 /**
  * see http://www.eclipse.org/Xtext/documentation/latest/xtext.html#contentAssist on how to customize content assistant
  */
@@ -39,71 +27,5 @@ public class CompleteOCLProposalProvider extends AbstractCompleteOCLProposalProv
 	@Override
 	public void complete_PrimitiveTypeIdentifier(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
 		proposeKeywordAlternatives(ruleCall, context, acceptor, getPrimitiveTypeImage());
-	}
-	
-	@Override
-	public void createProposals(ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
-		System.out.println("createProposals: ");
-		super.createProposals(context, acceptor);
-	}
-
-	@Override
-	protected void invokeMethod(String methodName, ICompletionProposalAcceptor acceptor, Object... params) {
-		System.out.println("  invokeMethod: " + methodName);
-		super.invokeMethod(methodName, acceptor, params);
-	}
-
-	@Override
-	protected void lookupCrossReference(CrossReference crossReference,
-			EReference reference, ContentAssistContext contentAssistContext,
-			ICompletionProposalAcceptor acceptor,
-			Predicate<IEObjectDescription> filter) {
-		EObject currentModel = contentAssistContext.getCurrentModel();
-		if (currentModel instanceof InfixExpCS) {
-			EObject previousModel = contentAssistContext.getPreviousModel();
-			if (previousModel instanceof NavigationOperatorCS) {
-				ExpCS argument = ((NavigationOperatorCS)previousModel).getArgument();
-				if (argument != null) {
-					currentModel = argument;
-				}
-			}
-		}
-		if (currentModel instanceof SimpleNamedElementRefCS) {
-/*			InternalEList<NamedElement> path = (InternalEList<NamedElement>) ((QualifiedElementRefCS)currentModel).getPath();
-			INode lastCompleteNode = contentAssistContext.getLastCompleteNode();
-			INode currentNode = contentAssistContext.getCurrentNode();
-			int nestedScopes = 0;
-			ICompositeNode parentNode = lastCompleteNode.getParent();
-			for (INode aChildNode : parentNode.getChildren()) {
-				if (aChildNode == currentNode) {
-					break;
-				}
-				if ((aChildNode instanceof ILeafNode) && "::".equals(aChildNode.getText())) {
-					NamedElement scope = path.basicGet(nestedScopes);
-					if (scope.eIsProxy()) {
-						break;
-					}
-					currentModel = scope;
-					nestedScopes++;
-				}
-			} */
-		}
-//		super.lookupCrossReference(crossReference, reference, contentAssistContext,
-//			acceptor, filter);
-		String ruleName = null;
-		if (crossReference.getTerminal() instanceof RuleCall) {
-			ruleName = ((RuleCall) crossReference.getTerminal()).getRule().getName();
-		}
-		lookupCrossReference(currentModel, reference, acceptor, filter,
-				getProposalFactory(ruleName, contentAssistContext));
-	}
-
-	@Override
-	protected void lookupCrossReference(EObject model, EReference reference,
-			ICompletionProposalAcceptor acceptor,
-			Predicate<IEObjectDescription> filter,
-			Function<IEObjectDescription, ICompletionProposal> proposalFactory) {
-		System.out.println("    lookupCrossReference: " + reference.getEContainingClass().getName() + "::" + reference.getName());
-		super.lookupCrossReference(model, reference, acceptor, filter, proposalFactory);
 	}
 }
--- examples/org.eclipse.ocl.examples.xtext.essentialocl.ui/src/org/eclipse/ocl/examples/xtext/essentialocl/ui/contentassist/EssentialOCLProposalProvider.java
+++ examples/org.eclipse.ocl.examples.xtext.essentialocl.ui/src/org/eclipse/ocl/examples/xtext/essentialocl/ui/contentassist/EssentialOCLProposalProvider.java
@@ -17,14 +17,26 @@
 package org.eclipse.ocl.examples.xtext.essentialocl.ui.contentassist;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.ocl.examples.pivot.PivotFactory;
+import org.eclipse.ocl.examples.xtext.base.baseCST.QualifiedNamedElementRefCS;
+import org.eclipse.ocl.examples.xtext.base.baseCST.SimpleNamedElementRefCS;
+import org.eclipse.ocl.examples.xtext.essentialocl.essentialOCLCST.ExpCS;
+import org.eclipse.ocl.examples.xtext.essentialocl.essentialOCLCST.InfixExpCS;
+import org.eclipse.ocl.examples.xtext.essentialocl.essentialOCLCST.NavigationOperatorCS;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.xtext.AbstractElement;
 import org.eclipse.xtext.Alternatives;
+import org.eclipse.xtext.CrossReference;
 import org.eclipse.xtext.Keyword;
 import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext;
 import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
+
+import com.google.common.base.Predicate;
 /**
  * see http://www.eclipse.org/Xtext/documentation/latest/xtext.html#contentAssist on how to customize content assistant
  */
@@ -65,12 +77,77 @@ public class EssentialOCLProposalProvider extends AbstractEssentialOCLProposalPr
 		proposeKeywordAlternatives(ruleCall, context, acceptor, getPrimitiveTypeImage());
 	}
 
+/*	@Override
+	public void createProposals(ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
+		System.out.println("createProposals: ");
+		super.createProposals(context, acceptor);
+	} */
+
+	protected EObject getPathScope(EObject model, ContentAssistContext context) {
+		int offset = context.getOffset();
+		INode currentNode = context.getCurrentNode();
+		INode offsetNode = NodeModelUtils.findLeafNodeAtOffset(currentNode, offset);
+		EObject eObject = NodeModelUtils.findActualSemanticObjectFor(offsetNode);
+		if (!(eObject instanceof SimpleNamedElementRefCS)) {
+			offsetNode = NodeModelUtils.findLeafNodeAtOffset(currentNode, offset-1);
+			eObject = NodeModelUtils.findActualSemanticObjectFor(offsetNode);
+		}
+		if (eObject instanceof SimpleNamedElementRefCS) {
+			model = eObject;
+		}
+		return model;
+	}
+
 	protected Image getPrimitiveTypeImage() {
 		if (primitiveTypeImage == null) {
 			primitiveTypeImage = getImage(PivotFactory.eINSTANCE.createPrimitiveType());
 		}
 		return primitiveTypeImage;
 	}
+
+/*	@Override
+	protected void invokeMethod(String methodName, ICompletionProposalAcceptor acceptor, Object... params) {
+		System.out.println("  invokeMethod: " + methodName);
+		super.invokeMethod(methodName, acceptor, params);
+	} */
+
+	@Override
+	protected void lookupCrossReference(CrossReference crossReference,
+			EReference reference, ContentAssistContext contentAssistContext,
+			ICompletionProposalAcceptor acceptor,
+			Predicate<IEObjectDescription> filter) {
+		EObject currentModel = contentAssistContext.getCurrentModel();
+		if (currentModel instanceof InfixExpCS) {
+			EObject previousModel = contentAssistContext.getPreviousModel();
+			if (previousModel instanceof NavigationOperatorCS) {
+				ExpCS argument = ((NavigationOperatorCS)previousModel).getArgument();
+				if (argument != null) {
+					currentModel = argument;
+				}
+			}
+		}
+//		else if (currentModel instanceof SimpleNamedElementRefCS) {
+//			currentModel = getPathScope(currentModel, contentAssistContext);
+//		}
+		else if (currentModel instanceof QualifiedNamedElementRefCS) {
+			currentModel = getPathScope(currentModel, contentAssistContext);
+		}
+		String ruleName = null;
+		if (crossReference.getTerminal() instanceof RuleCall) {
+			ruleName = ((RuleCall) crossReference.getTerminal()).getRule().getName();
+		}
+		lookupCrossReference(currentModel, reference, acceptor, filter,
+				getProposalFactory(ruleName, contentAssistContext));
+	}
+
+/*	@Override
+	protected void lookupCrossReference(EObject model, EReference reference,
+			ICompletionProposalAcceptor acceptor,
+			Predicate<IEObjectDescription> filter,
+			Function<IEObjectDescription, ICompletionProposal> proposalFactory) {
+		System.out.println("    lookupCrossReference: " + reference.getEContainingClass().getName() + "::" + reference.getName());
+		super.lookupCrossReference(model, reference, acceptor, filter, proposalFactory);
+	} */
 
 	protected void proposeKeywordAlternatives(RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor, Image image) {
 		AbstractElement alternatives = ruleCall.getRule().getAlternatives();
